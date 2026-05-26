11316ff61f0a82791d2bcf562bf1205a141d7387
Gastu9 <gaston.gravano@gmail.com>
2026-05-26 12:11:55 -0300

    no anda anda

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,18 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalyestudiante: number
+let totalycantidad: number
+
+if (esEstudiante="si"){
+    totalTexto / 0.9 = totalyestudiante
+    totalyestudiante / personasTexto = totalycantidad
+}
+else{
+    totalTexto = totalyestudiante
+    totalyestudiante / personasTexto = totalycantidad
+}
+
+let totalPorPersona: number = totalycantidad; // COMPLETAR
+
 console.log(`Total por persona: $${totalPorPersona}`);
